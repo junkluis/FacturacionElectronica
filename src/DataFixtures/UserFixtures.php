8823888e290a14785bc3e77c37e1644ccccc8035
Luis <lufezuro@gmail.com>
2020-05-26 13:44:51 -0500

    seguridad

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface
+
+class UserFixtures extends Fixture
+{
+
+	private $passwordEncoder;
+
+
+	public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername("luisz");
+        $user->setRoles(array("admin"));
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'testingUser'
+        ));
+
+        $manager->flush();
+    }
+}
