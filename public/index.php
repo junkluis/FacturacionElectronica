a49e19398ff3b6238e84704d57fc9978b7750851
Luis <lufezuro@gmail.com>
2020-05-26 10:23:02 -0500

    apache pack

--- public/index.php
+++ public/index.php
@@ -12,7 +12,8 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
+
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
 }
 
